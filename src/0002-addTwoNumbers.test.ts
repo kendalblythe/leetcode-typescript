6d02cf3d3ae6f826df138cdc2555246e592cbc10
Kendal Blythe <kendal.blythe@gmail.com>
2024-04-16 18:58:31 -0400

    reformat code

--- src/0002-addTwoNumbers.test.ts
+++ src/0002-addTwoNumbers.test.ts
@@ -1,24 +1,24 @@
 test("2. Add Two Numbers", () => {
-	expect(addTwoNumbers([2, 4, 3], [5, 6, 4])).toEqual([7, 0, 8]);
-	expect(addTwoNumbers([0], [0])).toEqual([0]);
-	expect(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])).toEqual([
-		8, 9, 9, 9, 0, 0, 0, 1,
-	]);
+  expect(addTwoNumbers([2, 4, 3], [5, 6, 4])).toEqual([7, 0, 8]);
+  expect(addTwoNumbers([0], [0])).toEqual([0]);
+  expect(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])).toEqual([
+    8, 9, 9, 9, 0, 0, 0, 1,
+  ]);
 });
 
 function addTwoNumbers(list1: number[], list2: number[]): number[] {
-	const num1 = listToNumber(list1);
-	const num2 = listToNumber(list2);
-	return numberToList(num1 + num2);
+  const num1 = listToNumber(list1);
+  const num2 = listToNumber(list2);
+  return numberToList(num1 + num2);
 }
 
 function listToNumber(list: number[]): number {
-	const num = list.reduce((prev, curr, index) => {
-		return prev + curr * 10 ** index;
-	}, 0);
-	return num;
+  const num = list.reduce((prev, curr, index) => {
+    return prev + curr * 10 ** index;
+  }, 0);
+  return num;
 }
 
 function numberToList(num: number): number[] {
-	return [...num.toString()].toReversed().map((s) => Number.parseInt(s));
+  return [...num.toString()].toReversed().map((s) => Number.parseInt(s));
 }
