aa3a9274f83f8cd9d4c66abc9332062b55e070fd
Kendal Blythe <kendal.blythe@gmail.com>
2024-05-27 13:00:57 -0400

    update problem 4

--- src/0004-findMedianSortedArrays.test.ts
+++ src/0004-findMedianSortedArrays.test.ts
@@ -7,21 +7,25 @@ test("4. Median of Two Sorted Arrays", () => {
 
 const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
   const mergedArrayLen = nums1.length + nums2.length;
-  const quotient = Math.floor(mergedArrayLen / 2);
-  const remainder = mergedArrayLen % 2;
-  const mergedArray: number[] = [];
-  for (let i = 0; i < mergedArrayLen; i++) {
+  const medianIndex = Math.floor(mergedArrayLen / 2);
+  const multipleMedianIndices = mergedArrayLen % 2 === 0;
+
+  // Get sorted merged array up to median index
+  const sortedMergedArray: number[] = [];
+  for (let i = 0; i <= medianIndex; i++) {
     const num1 = nums1[0];
     const num2 = nums2[0];
     if (num1 < num2) {
-      mergedArray.push(num1);
+      sortedMergedArray.push(num1);
       nums1.shift();
     } else {
-      mergedArray.push(num2);
+      sortedMergedArray.push(num2);
       nums2.shift();
     }
   }
-  return remainder === 0
-    ? (mergedArray[quotient - 1] + mergedArray[quotient]) / 2
-    : mergedArray[quotient];
+
+  // Return median value
+  return multipleMedianIndices
+    ? (sortedMergedArray[medianIndex - 1] + sortedMergedArray[medianIndex]) / 2
+    : sortedMergedArray[medianIndex];
 };
