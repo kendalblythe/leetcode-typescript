146740f2188660628df1b352d12228b8a1e1d4f7
Kendal Blythe <kendal.blythe@gmail.com>
2024-05-27 22:25:17 -0400

    add problem 5

---
+++ src/0005-longestPalindrome.test.ts
@@ -0,0 +1,17 @@
+test("5. Longest Palindromic Substring", () => {
+  expect(longestPalindrome("babad")).toEqual("bab");
+  expect(longestPalindrome("cbbd")).toEqual("bb");
+});
+
+const longestPalindrome = (s: string): string => {
+  const minSubstrLen = 2;
+  const maxSubstrLen = s.length - 1;
+  for (let substrLen = maxSubstrLen; substrLen >= minSubstrLen; substrLen--) {
+    for (let startIndex = 0; startIndex <= s.length - substrLen; startIndex++) {
+      const substr = s.substring(startIndex, startIndex + substrLen);
+      const reverseSubstr = [...substr].reverse().join("");
+      if (substr === reverseSubstr) return substr;
+    }
+  }
+  return "";
+};
