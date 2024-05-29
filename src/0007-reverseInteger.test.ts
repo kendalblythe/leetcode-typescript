35aaf55632635db1f127b6f698137aa2cf6ecabd
Kendal Blythe <kendal.blythe@gmail.com>
2024-05-28 22:44:36 -0400

    add problem 8

--- src/0007-reverseInteger.test.ts
+++ src/0007-reverseInteger.test.ts
@@ -2,11 +2,17 @@ test("7. Reverse Integer", () => {
   expect(reverseInteger(123)).toEqual(321);
   expect(reverseInteger(-123)).toEqual(-321);
   expect(reverseInteger(120)).toEqual(21);
+  expect(reverseInteger(9888777666)).toEqual(0);
+  expect(reverseInteger(-9888777666)).toEqual(0);
 });
+
+const MAX_VALUE = 2 ** 31 - 1;
+const MIN_VALUE = 2 ** 31 * -1;
 
 const reverseInteger = (n: number): number => {
   const s = n.toString();
   const reverseS = [...s].reverse().join("");
   const reverseN = Number.parseFloat(reverseS);
-  return n < 0 ? -reverseN : reverseN;
+  const num = n < 0 ? -reverseN : reverseN;
+  return num < MIN_VALUE || num > MAX_VALUE ? 0 : num;
 };
---
+++ src/0008-stringToInteger.test.ts
@@ -0,0 +1,47 @@
+test("8. String to integer atoi", () => {
+  expect(stringToInteger("42")).toEqual(42);
+  expect(stringToInteger(" -042")).toEqual(-42);
+  expect(stringToInteger("1337c0d3")).toEqual(1337);
+  expect(stringToInteger("0-1")).toEqual(0);
+  expect(stringToInteger("words and 987")).toEqual(0);
+  expect(stringToInteger("+9888777666")).toEqual(MAX_VALUE);
+  expect(stringToInteger("-9888777666")).toEqual(MIN_VALUE);
+});
+
+const MAX_VALUE = 2 ** 31 - 1;
+const MIN_VALUE = 2 ** 31 * -1;
+
+const stringToInteger = (s: string): number => {
+  let trimmedStr = trimLeadingWhitespace(s);
+  let isNegative = false;
+  if (trimmedStr.startsWith("+")) {
+    trimmedStr = trimmedStr.substring(1);
+  } else if (trimmedStr.startsWith("-")) {
+    isNegative = true;
+    trimmedStr = trimmedStr.substring(1);
+  }
+  let num = getLeadingDigits(trimmedStr);
+  num = isNegative ? -num : num;
+  return Math.max(Math.min(num, MAX_VALUE), MIN_VALUE);
+};
+
+const trimLeadingWhitespace = (s: string): string => {
+  let trimmedStr = s;
+  while (trimmedStr.match(/^\s/)) {
+    trimmedStr = trimmedStr.substring(1);
+  }
+  return trimmedStr;
+};
+
+const getLeadingDigits = (s: string): number => {
+  let digitsStr = "";
+  for (let i = 0; i < s.length; i++) {
+    const c = s.charAt(i);
+    if (c.match(/^[0-9]/)) {
+      digitsStr += c;
+    } else {
+      break;
+    }
+  }
+  return digitsStr ? Number.parseInt(digitsStr) : 0;
+};
